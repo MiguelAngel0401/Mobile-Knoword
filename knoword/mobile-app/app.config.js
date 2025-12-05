feb0af7a5fbd130288b262ca3a19b9d635c1d5a1
Your Name <islasmiguel737@gmail.com>
2025-12-05 08:53:22 -0600

    Fix package-lock.json

--- knoword/mobile-app/app.config.js
+++ knoword/mobile-app/app.config.js
@@ -2,12 +2,18 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "AlphaEdu",
+    name: "Knoword",
     slug: "alphaedu",
     version: "1.0.0",
     sdkVersion: "54.0.0",
     icon: "./assets/images/logo.png",
+    android: {
+      package: "com.knoword.app"
+    },
     extra: {
+      eas: {
+        projectId: "b6cab36f-a45f-4f68-850c-3b08d2bd5f2b"
+      },
       CLOUDINARY_CLOUD_NAME: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
       CLOUDINARY_UPLOAD_PRESET: process.env.EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
     },
